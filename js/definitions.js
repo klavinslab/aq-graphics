032afe4e05bd5e07674f3feb60a0c11fb13ca88a
Justin Vrana <justin.vrana@gmail.com>
2020-05-15 07:09:21 -0700

    Aquadoc initial commit

---
+++ js/definitions.js
@@ -0,0 +1,77 @@
+var config = {
+
+  tagline: "The Laboratory</br>Operating System",
+  documentation_url: "http://localhost:4000/aquarium",
+  title: "Aquarium SVG Graphics Library",
+  navigation: [
+
+    {
+      category: "Overview",
+      contents: [
+        { name: "Introduction", type: "local-md", path: "README.md" },
+        { name: "About this Workflow", type: "local-md", path: "ABOUT.md" },
+        { name: "License", type: "local-md", path: "LICENSE.md" },
+        { name: "Issues", type: "external-link", path: 'https://github.com/klavinslab/aq-graphics/issues' }
+      ]
+    },
+
+    
+
+      {
+
+        category: "Operation Types",
+
+        contents: [
+
+          
+
+        ]
+
+      },
+
+    
+
+    
+
+      {
+
+        category: "Libraries",
+
+        contents: [
+
+          
+            {
+              name: 'GraphicsBase',
+              path: 'libraries/GraphicsBase' + '.html',
+              type: "local-webpage"
+            },
+          
+            {
+              name: 'SVGGraphicsImages',
+              path: 'libraries/SVGGraphicsImages' + '.html',
+              type: "local-webpage"
+            },
+          
+
+        ]
+
+    },
+
+    
+
+    
+      { category: "Sample Types",
+        contents: [
+          
+        ]
+      },
+      { category: "Containers",
+        contents: [
+          
+        ]
+      }
+    
+
+  ]
+
+};
